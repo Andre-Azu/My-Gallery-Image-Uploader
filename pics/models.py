4d2d6aaa16c81e3a2081ea4b47b570898fe1cd44
andre azu <andreazuaa@gmail.com>
2021-12-03 10:45:53 +0300

    add first migration

--- pics/models.py
+++ pics/models.py
@@ -3,3 +3,5 @@ from django.db import models
 # Create your models here.
 class image(models.Model):
     title = models.CharField(max_length=60)
+    description = models.CharField(max_length=400)
+
