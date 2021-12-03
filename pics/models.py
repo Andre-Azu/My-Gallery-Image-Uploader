b4e3dc761839cfab8e7bdb1a4d861b708cab399d
andre azu <andreazuaa@gmail.com>
2021-12-03 11:13:56 +0300

    add image

--- pics/models.py
+++ pics/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class image(models.Model):
     title = models.CharField(max_length=60)
     description = models.CharField(max_length=400)
-
+    image = models.ImageField(upload_to = 'image/')
